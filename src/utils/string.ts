6efa5f6ea7a2cf08049e5ae3968259baadf4e0b6
Zohaib Tariq <zohaibtariq@users.noreply.github.com>
2023-09-15 21:02:35 +0500

    Updates in development

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,35 +1,3 @@
-import {Request, Response, NextFunction} from "express";
-import jwt from "jsonwebtoken";
-
-interface UserPayload {
-    id: string,
-    email: string
-}
-
-declare global {
-    namespace  Express {
-        interface Request {
-            currentUser?: UserPayload
-        }
-    }
-}
-
-export const currentUser = (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    if(!req.session?.jwt)
-        return next()
-    try{
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload
-        req.currentUser = payload
-    }catch (e) {
-    }
-    next()
-}
-
-
 export const generateRandomString = (length: number = 10): string => {
     const characters: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result: string = '';
